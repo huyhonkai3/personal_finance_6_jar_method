5e006f98250b77753ca90101e1eae2d33e12c315
huyhonkai3 <130666523+huyhonkai3@users.noreply.github.com>
2026-08-18 03:28:51 +0700

    feat: export phase 7 shared schemas

--- shared/schemas/index.js
+++ shared/schemas/index.js
@@ -1,6 +1,8 @@
-// Export gop toan bo schema, dung chung boi ca client (React Hook Form)
-// va server (validate.middleware.js)
+// Export gộp toàn bộ schema dùng chung client/server.
 export * from "./jar.schema.js";
 export * from "./income.schema.js";
 export * from "./transaction.schema.js";
 export * from "./transfer.schema.js";
+export * from "./notification.schema.js";
+export * from "./period.schema.js";
+export * from "./debt.schema.js";
